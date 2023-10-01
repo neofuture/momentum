21cca170a9b87cc9daef4bb8a00573e594743ff2
Carl Fearby <carlfearby@me.com>
2023-10-01 14:58:47 +0100

    Small refactor

--- src/app/bloks/nestable/search-bar-blok/search-bar-blok.component.ts
+++ src/app/bloks/nestable/search-bar-blok/search-bar-blok.component.ts
@@ -1,10 +1,7 @@
 import {Component, HostBinding, OnInit} from "@angular/core";
-import {AsyncPipe, CurrencyPipe, JsonPipe, NgForOf, NgIf} from "@angular/common";
-import {StoryblokBlokDirective, StoryblokRenderDirective} from "@/storyblok";
-import {HttpClient, HttpClientModule} from "@angular/common/http";
-import {environment} from "../../../../environments/environment";
-import {FormsModule} from "@angular/forms";
-import {LoadingComponent} from "@/components/loading";
+import {NestableBlok, SbNestableBlok, StoryblokBlokDirective, StoryblokRenderDirective} from "@/storyblok";
+import {SearchBarBlok} from "@/bloks/nestable/search-bar-blok/search-bar-blok.interface";
+import {SearchBarComponent} from "@/components/search-bar/search-bar.component";
 
 @Component({
     selector: 'app-search-bar-blok',
@@ -12,84 +9,16 @@ import {LoadingComponent} from "@/components/loading";
     styleUrls: ['./search-bar-blok.component.css'],
     standalone: true,
     imports: [
-        JsonPipe,
-        AsyncPipe,
-        CurrencyPipe,
         StoryblokRenderDirective,
-        NgForOf,
-        NgIf,
-        HttpClientModule,
-        FormsModule,
-        LoadingComponent
+        SearchBarComponent,
     ],
     hostDirectives: [{
         directive: StoryblokBlokDirective,
         inputs: ['blok']
     }],
 })
-export class SearchBarBlokComponent implements OnInit {
+export class SearchBarBlokComponentr {
     @HostBinding('class.search-bar')
     protected readonly hbClass = true;
-    inputFocus: boolean = false;
-    searchText: string = '';
-    searchDeBounce: any;
-    listings: any = [];
-    searchLoading = false;
 
-    constructor(
-        private httpClient: HttpClient
-    ) {
-    }
-
-    ngOnInit() {
-    }
-
-    setFocus() {
-        this.inputFocus = true;
-    }
-
-    setBlur() {
-        this.inputFocus = false;
-    }
-
-    search(event: any) {
-        clearTimeout(this.searchDeBounce);
-
-        if (event.key === 'Escape') {
-            setTimeout(() => {
-                this.listings = [];
-            }, 500);
-            this.searchLoading = false;
-            event.target.blur();
-        }
-
-        if (event.key === 'Enter') {
-            this.listings = [];
-            this.searchLoading = false;
-            alert('Search for ' + event.target.value);
-        } else {
-            this.searchDeBounce = setTimeout(() => {
-                let searchValue = event.target.value;
-                this.searchLoading = true;
-                this.listings = [];
-                if (searchValue.length > 2) {
-                    this.searchLoading = true;
-                    this.httpClient.get(`${environment.api}/search?search=${searchValue}`).subscribe((data: any) => {
-                        this.listings = data.data.listings;
-                        this.searchLoading = false;
-                    });
-                } else {
-                    this.listings = [];
-                    this.searchLoading = false;
-                }
-            }, 300);
-        }
-    }
-
-    selectListing(listing: any) {
-        this.listings = [];
-        const listing_title = listing.listing_title.replace(/ /g, '-').replace(/%/g, '').replace(/(\(|\))/g, '');
-        const url  = `/item/${listing.listing_id}/${listing_title}`;
-        window.location.href = url;
-    }
 }
---
+++ src/app/components/search-bar/search-bar.component.ts
@@ -0,0 +1,84 @@
+import {Component} from "@angular/core";
+import {CurrencyPipe, NgForOf, NgIf} from "@angular/common";
+import {LoadingComponent} from "@/components/loading";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {environment} from "../../../environments/environment";
+import {HttpClient, HttpClientModule} from "@angular/common/http";
+
+@Component({
+    selector: 'app-search-bar',
+    templateUrl: './search-bar.component.html',
+    styleUrls: ['./search-bar.component.css'],
+    imports: [
+        CurrencyPipe,
+        LoadingComponent,
+        NgForOf,
+        NgIf,
+        ReactiveFormsModule,
+        HttpClientModule,
+        FormsModule,
+    ],
+    standalone: true
+
+
+})
+
+export class SearchBarComponent {
+    inputFocus: boolean = false;
+    searchText: string = '';
+    searchDeBounce: any;
+    listings: any = [];
+    searchLoading = false;
+    constructor(
+        private httpClient: HttpClient
+    ) {
+    }
+    setFocus() {
+        this.inputFocus = true;
+    }
+
+    setBlur() {
+        this.inputFocus = false;
+    }
+
+    search(event: any) {
+        clearTimeout(this.searchDeBounce);
+
+        if (event.key === 'Escape') {
+            setTimeout(() => {
+                this.listings = [];
+            }, 500);
+            this.searchLoading = false;
+            event.target.blur();
+        }
+
+        if (event.key === 'Enter') {
+            this.listings = [];
+            this.searchLoading = false;
+            alert('Search for ' + event.target.value);
+        } else {
+            this.searchDeBounce = setTimeout(() => {
+                let searchValue = event.target.value;
+                this.searchLoading = true;
+                this.listings = [];
+                if (searchValue.length > 2) {
+                    this.searchLoading = true;
+                    this.httpClient.get(`${environment.api}/search?search=${searchValue}`).subscribe((data: any) => {
+                        this.listings = data.data.listings;
+                        this.searchLoading = false;
+                    });
+                } else {
+                    this.listings = [];
+                    this.searchLoading = false;
+                }
+            }, 300);
+        }
+    }
+
+    selectListing(listing: any) {
+        this.listings = [];
+        const listing_title = listing.listing_title.replace(/ /g, '-').replace(/%/g, '').replace(/(\(|\))/g, '');
+        const url  = `/item/${listing.listing_id}/${listing_title}`;
+        window.location.href = url;
+    }
+}
